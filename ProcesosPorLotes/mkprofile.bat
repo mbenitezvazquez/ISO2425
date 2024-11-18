2229da18d990b6c718f920adcf9d593bc0b26938
mbenitezvazquez <mbenitezv01@iescastelar.com>
2024-11-18 09:56:47 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Script de prueba
+:: Autor: Mario Benítez Vázquez
+:: Fecha: 18-11-2024
+
+echo Hola
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract  %USERPROFILE%\asir\examen
+
+mkdir %USERPROFILE%\daw\docs  %USERPROFILE%\daw\pract  %USERPROFILE%\daw\examen
+
+mkdir %USERPROFILE%\dam\docs  %USERPROFILE%\dam\pract  %USERPROFILE%\dam\examen
+
+mkdir %USERPROFILE%\smr\docs  %USERPROFILE%\smr\pract  %USERPROFILE%\smr\examen
