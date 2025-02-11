fdd3ebd49c232dda4f983b98bc73dc2a91840c19
mbenitezvazquez <mbenitezv01@iescastelar.com>
2025-02-11 10:20:43 +0100

    Update fiveOrMore.sh

--- scripts/fiveOrMore.sh
+++ scripts/fiveOrMore.sh
@@ -18,39 +18,72 @@ clear
 		if [ -f $1 ]; then
 
 			echo "Fichero Existente"
+			exit
 		else
 
 			echo "Fichero Inexistente"
-			exit
 
 		fi
 
 
 		if [ -d $2 ]; then
 
-			echo "Fichero Existente"
+			echo "Directorio Existente"
 		else
 
-			echo "Fichero Inexistente"
+			echo "Directorio Inexistente"
 			exit
 		fi
 
 
 		if [ -z $2 ]; then
-			echo "No esta vacio"
+			echo "Ficero vacio"
 
 		else
-			echo "fichero vacio"
-			exit
+			echo "No esta  vacio"
+
 
 		fi
 
 
-	echo "Autor: Mario Benítez Vázquez"
+	echo "Mario Benítez Vázquez"
 
 
-		for i in $2/*.txt | -wc -l; do
+		archivo=$(ls $2 | wc -l)
 
-			echo $i
+                if [ $archivo -eq 0 ]; then
 
+                echo "Directorio $2 vacio"
+                exit
+                fi
+
+
+
+
+		for i in $2/*.txt; do
+
+			lineas=$(cat $i | wc -l)
+
+			if [ $lineas -ge 5 ]; then
+
+				echo $i
+				echo $i >> $1
+
+				palabras=$(cat $i | wc -w)
+				echo "El fichero original teine $palabras palabra" >> $1.q
+				cat $1 > $i.q
+
+				echo "creando... "
+				ls $2
+
+			fi
 		done
+
+
+
+
+		num=$(cat $1 | wc -l)
+		echo "El fichero $1 $num ficheros" >> $1
+
+
+
